5f119918234f4116b1469817bc8993cad05690cb
rgrnwd <rgrnwd@thoughtworks.com>
2019-06-22 22:28:20 +1000

    optional illustration option

--- app.rb
+++ app.rb
@@ -6,10 +6,12 @@ illustrator = Illustrator.new()
 
 puts 'Welcome to the Overflow Tower!'
 puts '==============================='
+print 'Do you want to run with illustrations? (y/n) '
+show_illustrations = gets.chomp == 'y'
 puts 'There is a stack of water glasses in a form of triangle.'
 
 glass_stack = console_interface.build_glass_stack_from_input
-illustrator.print_stack(glass_stack)
+illustrator.print_stack(glass_stack) if show_illustrations
 
 puts 'When a liquid is poured into the top most glass any overflow is evenly '\
 'distributed between the glasses in the next row.'
@@ -17,7 +19,7 @@ puts 'That is, half of the overflow pours into the left glass while the remainde
 'of the overflow pours into the right glass.'
 
 console_interface.pour_water_onto_stack(glass_stack)
-illustrator.print_stack(glass_stack)
+illustrator.print_stack(glass_stack) if show_illustrations
 
 loop do
     console_interface.display_glass_content(glass_stack)
